1db606b6a3a3f173d2eb0a06240ca5c13d8cf8d0
hyuk0608 <hyeok0608@gmail.com>
2020-11-02 13:08:39 +0900

    userinfobean+service+userinfo수정

--- src/main/java/co/soft/controller/UserController.java
+++ src/main/java/co/soft/controller/UserController.java
@@ -6,10 +6,12 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import co.soft.domain.UserInfoBean;
 import co.soft.service.UserService;
@@ -40,4 +42,10 @@ public class UserController {
 	userService.insertUser(joinUserBean);
 	return "redirct:/user/join_success";
 	}
+	
+	@RequestMapping("/getUserInfoList")
+	public String getUserInfoList(Model model, UserInfoBean userinfo) {
+		return "userinfo";
+	}
+	
 }
