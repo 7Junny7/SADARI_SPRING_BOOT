f2bf48b31d0f198c774f0b93ca77cb8aae9eebf2
7Junny7 <panthe85@gmail.com>
2020-11-04 17:03:49 +0900

    20201104-17:00 map 작업중

--- src/main/java/co/soft/controller/MainController.java
+++ src/main/java/co/soft/controller/MainController.java
@@ -9,8 +9,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import co.soft.domain.MapInfoBean;
 import co.soft.domain.MenuInfoBean;
 import co.soft.domain.UserInfoBean;
+import co.soft.service.MapService;
 import co.soft.service.MenuService;
 
 @Controller
@@ -18,13 +20,13 @@ import co.soft.service.MenuService;
 public class MainController {
 
 	@Autowired
-	@Lazy
-	private MenuService menuService;
+	private MapService mapService;
 
 	@GetMapping("/home") // OK
-	public String getMenuList(Model model, MenuInfoBean menuinfo, UserInfoBean userinfo) {
-//		List<MenuInfoBean> menuList = menuService.getMenuList(menuinfo);
-//		model.addAttribute("menuList", menuList);
+	public String getMenuList(Model model, MapInfoBean mapinfo, UserInfoBean userinfo) {
+		List<MapInfoBean> mapList = mapService.getMapList(mapinfo);
+		model.addAttribute("mapList", mapList);
+		
 		model.addAttribute("loginUserBean", userinfo);
 		return "home";
 	}
