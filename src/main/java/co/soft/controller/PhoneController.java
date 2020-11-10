66c8c695866d299aec83734f93ab9cdbd3471fe7
yugangchoe <eqlscl5@naver.com>
2020-11-10 15:29:55 +0900

    문자인증

--- src/main/java/co/soft/controller/PhoneController.java
+++ src/main/java/co/soft/controller/PhoneController.java
@@ -6,28 +6,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import co.soft.service.PhoneService;
+import co.soft.service.certificationService;
 
 @Controller
-public class PhoneController {
+public class PhoneController extends certificationService{
+	@GetMapping("/sendSMS")
+	 public @ResponseBody
+	    String sendSMS(String phoneNumber) {
 
-	
-	
-	@GetMapping("/check/sendSMS")
-    public @ResponseBody
-    String sendSMS(String phoneNumber) {
+	        Random rand  = new Random();
+	        String numStr = "";
+	        for(int i=0; i<4; i++) {
+	            String ran = Integer.toString(rand.nextInt(10));
+	            numStr+=ran;
+	        }
 
-        Random rand  = new Random();
-        String numStr = "";
-        for(int i=0; i<4; i++) {
-            String ran = Integer.toString(rand.nextInt(10));
-            numStr+=ran;
-        }
-
-        System.out.println("수신자 번호 : " + phoneNumber);
-        System.out.println("인증번호 : " + numStr);
-        PhoneService.certifiedPhoneNumber(phoneNumber,numStr);
-        return numStr;
-    }
-	
+	        System.out.println("수신자 번호 : " + phoneNumber);
+	        System.out.println("인증번호 : " + numStr);
+	        certifiedPhoneNumber(phoneNumber, numStr);
+	        return numStr;
+	    }
 }
---
+++ src/main/java/co/soft/service/certificationService.java
@@ -0,0 +1,33 @@
+package co.soft.service;
+
+
+import java.util.HashMap;
+import org.json.simple.JSONObject;
+import org.springframework.stereotype.Service;
+
+import net.nurigo.java_sdk.api.Message;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
+@Service
+public class certificationService {
+	public void certifiedPhoneNumber(String phoneNumber, String cerNum) {
+        String api_key = "NCSLHGS73Y1I5F1K";
+        String api_secret = "JNGMSUCTTTIKB8ANG91E933YRULVRRBJ";
+        Message coolsms = new Message(api_key, api_secret);
+
+        // 4 params(to, from, type, text) are mandatory. must be filled
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("to", phoneNumber);    // 수신전화번호
+        params.put("from", phoneNumber);    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
+        params.put("type", "SMS");
+        params.put("text","["+cerNum+"]");
+//        params.put("app_version", "test app 1.2"); // application name and version
+
+        try {
+            JSONObject obj = (JSONObject) coolsms.send(params);
+            System.out.println(obj.toString());
+        } catch (CoolsmsException e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getCode());
+        }
+    }
+}
